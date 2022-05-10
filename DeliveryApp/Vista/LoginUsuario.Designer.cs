90333965c794ef6dd50ac2258c44f67f9a3bb83a
uliwrok <uliwrok@gmail.com>
2022-05-09 18:52:41 -0600

    qDGR

--- DeliveryApp/Vista/LoginUsuario.Designer.cs
+++ DeliveryApp/Vista/LoginUsuario.Designer.cs
@@ -38,8 +38,6 @@
             this.panel3 = new System.Windows.Forms.Panel();
             this.panel2 = new System.Windows.Forms.Panel();
             this.lblMensaje = new System.Windows.Forms.Label();
-            this.button_WOC2 = new ePOSOne.btnProduct.Button_WOC();
-            this.button_WOC1 = new ePOSOne.btnProduct.Button_WOC();
             this.label3 = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.panel1.SuspendLayout();
@@ -51,9 +49,10 @@
             // 
             this.button1.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.button1.ForeColor = System.Drawing.Color.Black;
-            this.button1.Location = new System.Drawing.Point(232, 368);
+            this.button1.Location = new System.Drawing.Point(174, 299);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(115, 51);
+            this.button1.Size = new System.Drawing.Size(86, 41);
             this.button1.TabIndex = 0;
             this.button1.Text = "Login";
             this.button1.UseVisualStyleBackColor = true;
@@ -62,9 +61,10 @@
             // txtcorreo
             // 
             this.txtcorreo.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.txtcorreo.Location = new System.Drawing.Point(210, 242);
+            this.txtcorreo.Location = new System.Drawing.Point(158, 197);
+            this.txtcorreo.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtcorreo.Name = "txtcorreo";
-            this.txtcorreo.Size = new System.Drawing.Size(195, 22);
+            this.txtcorreo.Size = new System.Drawing.Size(147, 20);
             this.txtcorreo.TabIndex = 1;
             // 
             // label1
@@ -72,9 +72,10 @@
             this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label1.AutoSize = true;
             this.label1.ForeColor = System.Drawing.Color.Black;
-            this.label1.Location = new System.Drawing.Point(153, 245);
+            this.label1.Location = new System.Drawing.Point(115, 199);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(48, 16);
+            this.label1.Size = new System.Drawing.Size(38, 13);
             this.label1.TabIndex = 2;
             this.label1.Text = "Correo";
             // 
@@ -83,18 +84,20 @@
             this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label2.AutoSize = true;
             this.label2.ForeColor = System.Drawing.Color.Black;
-            this.label2.Location = new System.Drawing.Point(123, 293);
+            this.label2.Location = new System.Drawing.Point(92, 238);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(76, 16);
+            this.label2.Size = new System.Drawing.Size(61, 13);
             this.label2.TabIndex = 4;
             this.label2.Text = "Contraseña";
             // 
             // txtCon
             // 
             this.txtCon.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.txtCon.Location = new System.Drawing.Point(210, 293);
+            this.txtCon.Location = new System.Drawing.Point(158, 238);
+            this.txtCon.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtCon.Name = "txtCon";
-            this.txtCon.Size = new System.Drawing.Size(195, 22);
+            this.txtCon.Size = new System.Drawing.Size(147, 20);
             this.txtCon.TabIndex = 3;
             // 
             // panel1
@@ -103,26 +106,26 @@
             this.panel1.BackColor = System.Drawing.Color.White;
             this.panel1.Controls.Add(this.panel3);
             this.panel1.Controls.Add(this.lblMensaje);
-            this.panel1.Controls.Add(this.button_WOC2);
             this.panel1.Controls.Add(this.label2);
-            this.panel1.Controls.Add(this.button_WOC1);
             this.panel1.Controls.Add(this.txtCon);
             this.panel1.Controls.Add(this.label1);
             this.panel1.Controls.Add(this.button1);
             this.panel1.Controls.Add(this.txtcorreo);
             this.panel1.ForeColor = System.Drawing.Color.White;
-            this.panel1.Location = new System.Drawing.Point(688, 166);
+            this.panel1.Location = new System.Drawing.Point(516, 135);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(544, 749);
+            this.panel1.Size = new System.Drawing.Size(408, 609);
             this.panel1.TabIndex = 6;
             // 
             // panel3
             // 
             this.panel3.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.panel3.Controls.Add(this.panel2);
-            this.panel3.Location = new System.Drawing.Point(0, 10);
+            this.panel3.Location = new System.Drawing.Point(0, 8);
+            this.panel3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(543, 152);
+            this.panel3.Size = new System.Drawing.Size(407, 124);
             this.panel3.TabIndex = 11;
             // 
             // panel2
@@ -130,9 +133,10 @@
             this.panel2.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.panel2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel2.BackgroundImage")));
             this.panel2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.panel2.Location = new System.Drawing.Point(113, 73);
+            this.panel2.Location = new System.Drawing.Point(85, 59);
+            this.panel2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(46, 46);
+            this.panel2.Size = new System.Drawing.Size(34, 37);
             this.panel2.TabIndex = 8;
             this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
             // 
@@ -142,46 +146,11 @@
             this.lblMensaje.AutoSize = true;
             this.lblMensaje.BackColor = System.Drawing.Color.WhiteSmoke;
             this.lblMensaje.ForeColor = System.Drawing.Color.Black;
-            this.lblMensaje.Location = new System.Drawing.Point(207, 330);
+            this.lblMensaje.Location = new System.Drawing.Point(155, 268);
+            this.lblMensaje.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblMensaje.Name = "lblMensaje";
-            this.lblMensaje.Size = new System.Drawing.Size(0, 16);
+            this.lblMensaje.Size = new System.Drawing.Size(0, 13);
             this.lblMensaje.TabIndex = 5;
-            // 
-            // button_WOC2
-            // 
-            this.button_WOC2.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.button_WOC2.BorderColor = System.Drawing.Color.WhiteSmoke;
-            this.button_WOC2.ButtonColor = System.Drawing.Color.WhiteSmoke;
-            this.button_WOC2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button_WOC2.ForeColor = System.Drawing.Color.Transparent;
-            this.button_WOC2.Location = new System.Drawing.Point(210, 466);
-            this.button_WOC2.Name = "button_WOC2";
-            this.button_WOC2.OnHoverBorderColor = System.Drawing.Color.Gray;
-            this.button_WOC2.OnHoverButtonColor = System.Drawing.Color.Yellow;
-            this.button_WOC2.OnHoverTextColor = System.Drawing.Color.Gray;
-            this.button_WOC2.Size = new System.Drawing.Size(160, 42);
-            this.button_WOC2.TabIndex = 10;
-            this.button_WOC2.Text = "Correo electrónico";
-            this.button_WOC2.TextColor = System.Drawing.Color.FromArgb(((int)(((byte)(112)))), ((int)(((byte)(112)))), ((int)(((byte)(112)))));
-            this.button_WOC2.UseVisualStyleBackColor = true;
-            this.button_WOC2.Click += new System.EventHandler(this.button_WOC2_Click);
-            // 
-            // button_WOC1
-            // 
-            this.button_WOC1.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.button_WOC1.BorderColor = System.Drawing.Color.Silver;
-            this.button_WOC1.ButtonColor = System.Drawing.Color.Red;
-            this.button_WOC1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button_WOC1.Location = new System.Drawing.Point(208, 542);
-            this.button_WOC1.Name = "button_WOC1";
-            this.button_WOC1.OnHoverBorderColor = System.Drawing.Color.Gray;
-            this.button_WOC1.OnHoverButtonColor = System.Drawing.Color.Yellow;
-            this.button_WOC1.OnHoverTextColor = System.Drawing.Color.Gray;
-            this.button_WOC1.Size = new System.Drawing.Size(160, 56);
-            this.button_WOC1.TabIndex = 9;
-            this.button_WOC1.Text = "Inicio de sesión";
-            this.button_WOC1.TextColor = System.Drawing.Color.White;
-            this.button_WOC1.UseVisualStyleBackColor = true;
             // 
             // label3
             // 
@@ -189,9 +158,10 @@
             this.label3.AutoSize = true;
             this.label3.BackColor = System.Drawing.Color.White;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(845, 242);
+            this.label3.Location = new System.Drawing.Point(634, 197);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(295, 58);
+            this.label3.Size = new System.Drawing.Size(234, 46);
             this.label3.TabIndex = 7;
             this.label3.Text = "DeliveryApp";
             // 
@@ -200,20 +170,22 @@
             this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
             this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(1902, 1033);
+            this.pictureBox1.Size = new System.Drawing.Size(1426, 839);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
             this.pictureBox1.TabIndex = 8;
             this.pictureBox1.TabStop = false;
             // 
             // LoginUsuario
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1902, 1033);
+            this.ClientSize = new System.Drawing.Size(1426, 839);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "LoginUsuario";
             this.Text = "Login";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
@@ -237,8 +209,8 @@
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Panel panel2;
-        private ePOSOne.btnProduct.Button_WOC button_WOC2;
-        private ePOSOne.btnProduct.Button_WOC button_WOC1;
+        //private ePOSOne.btnProduct.Button_WOC button_WOC2;
+        //private ePOSOne.btnProduct.Button_WOC button_WOC1;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Label lblMensaje;
         private System.Windows.Forms.Panel panel3;
