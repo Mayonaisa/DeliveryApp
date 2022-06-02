f5da900f140076cf409e6893411d1858e80ef1d9
Mayonaisa <kou4_alan@hotmail.com>
2022-06-02 17:03:33 -0600

    Cambios corregidos 1.0

--- DeliveryApp/Vista/MenuRecep.Designer.cs
+++ DeliveryApp/Vista/MenuRecep.Designer.cs
@@ -91,10 +91,10 @@
             // 
             this.panel1.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(153)))), ((int)(((byte)(84)))));
-            this.panel1.Location = new System.Drawing.Point(70, 90);
+            this.panel1.Location = new System.Drawing.Point(70, 87);
             this.panel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1460, 65);
+            this.panel1.Size = new System.Drawing.Size(1482, 68);
             this.panel1.TabIndex = 7;
             // 
             // label1
