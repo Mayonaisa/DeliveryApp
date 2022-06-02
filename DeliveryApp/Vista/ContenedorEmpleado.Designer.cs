0fbecc8eceb998ffd8316039f75788d79d61c405
Mayonaisa <kou4_alan@hotmail.com>
2022-06-02 02:43:41 -0600

    Admin 4.0

--- DeliveryApp/Vista/ContenedorEmpleado.Designer.cs
+++ DeliveryApp/Vista/ContenedorEmpleado.Designer.cs
@@ -232,7 +232,7 @@
             this.lblUsuario.AutoSize = true;
             this.lblUsuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblUsuario.ForeColor = System.Drawing.Color.White;
-            this.lblUsuario.Location = new System.Drawing.Point(52, 5);
+            this.lblUsuario.Location = new System.Drawing.Point(46, 5);
             this.lblUsuario.Name = "lblUsuario";
             this.lblUsuario.Size = new System.Drawing.Size(173, 29);
             this.lblUsuario.TabIndex = 1;
