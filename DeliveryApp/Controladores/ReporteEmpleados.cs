b547c1369b8620ebae5dd617ba6125c25027224f
Mayonaisa <kou4_alan@hotmail.com>
2022-06-02 20:07:59 -0600

    XD 4.0

---
+++ DeliveryApp/Controladores/ReporteEmpleados.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DeliveryApp.Modelos;
+
+namespace DeliveryApp.Controladores
+{
+    public class ReporteEmpleados
+    {
+        public void ReporteRecepcionista() 
+        {
+
+        }
+    }
+}
